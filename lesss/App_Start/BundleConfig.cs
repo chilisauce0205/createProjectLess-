db8e52ec80207876cb20903859688c2eda2594dd
Tai Nguyen <nguyenthanhtai168@gmail.com>
2017-08-26 10:22:29 +0700

    creae

--- lesss/App_Start/BundleConfig.cs
+++ lesss/App_Start/BundleConfig.cs
@@ -26,6 +26,7 @@ namespace lesss
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new LessBundle("~/Content/less").Include("~/Content/*.less"));
         }
     }
 }
