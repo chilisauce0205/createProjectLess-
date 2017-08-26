de11bdc82a37f8ceaaec440016f1c8f169cbcc4b
Tai Nguyen <nguyenthanhtai168@gmail.com>
2017-08-26 09:07:55 +0700

    Add project files.

---
+++ lesss/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lesss.Startup))]
+namespace lesss
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
